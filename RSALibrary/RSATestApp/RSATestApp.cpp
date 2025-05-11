2b2bc0c56fea9ee5f12a37ebd925eaf3afd63249
1337sc <46076326+1337sc@users.noreply.github.com>
2025-05-11 17:19:56 +0300

    Add dynamic LoadLibrary

--- RSALibrary/RSATestApp/RSATestApp.cpp
+++ RSALibrary/RSATestApp/RSATestApp.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 #include <cmath>
-#include "../RSALibrary/rsa_lib.h"  // Шлях до вашого заголовочного файлу DLL
-#pragma comment(lib, "../x64/Debug/RSALibrary.lib")
+#include "Windows.h"
+//#include "../RSALibrary/rsa_lib.h"  // Шлях до вашого заголовочного файлу DLL
+//#pragma comment(lib, "../x64/Debug/RSALibrary.lib")
+
+struct RSAKey {
+    long long exponent;
+    long long modulus;
+};
+
+typedef void (*generateKeys)(RSAKey*, RSAKey*, long long, long long);
+typedef long long (*encrypt)(long long, RSAKey);
+typedef long long (*decrypt)(long long, RSAKey);
 
 int main() {
+    HINSTANCE hLib = LoadLibrary(L"RSALibrary.dll");
+    if (!hLib) {
+        std::cerr << "Cannot load DLL\n";
+        return 1;
+    }
+
+    auto generateKeysImpl = (generateKeys)GetProcAddress(hLib, "generateKeys");
+    auto encryptImpl = (encrypt)GetProcAddress(hLib, "encrypt");
+    auto decryptImpl = (decrypt)GetProcAddress(hLib, "decrypt");
+
     RSAKey publicKey, privateKey;
-    generateKeys(&publicKey, &privateKey, 3, 7);
+    generateKeysImpl(&publicKey, &privateKey, 3, 7);
     
-    long long encrypted = encrypt(12, publicKey);
+    long long encrypted = encryptImpl(12, publicKey);
     std::cout << "Encrypted: " << encrypted << std::endl;
 
-    long long decrypted = decrypt(encrypted, privateKey);
+    long long decrypted = decryptImpl(encrypted, privateKey);
     std::cout << "Decrypted: " << decrypted << std::endl;
 
     return 0;
